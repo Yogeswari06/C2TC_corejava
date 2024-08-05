4b8fac52608a2f478a68d66ceae8f07d3cfc162c
ELCOT <ELCOT@Lenovo>
2024-08-05 09:55:52 -1100

    Encap

---
+++ Encapsulation/src/daythree/OopsConceptDemo.java
@@ -0,0 +1,31 @@
+package daythree;
+
+public class OopsConceptDemo {
+
+	public static void main(String[] args) {
+		private int serialNum;
+		private String namee;
+		private int age;
+		public int getserialNum() {
+			return serialNum;
+		}
+		public void setSerialNum(int serialNum) {
+			this.serialNum = serialNum;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public int getAge() {
+			return age;
+		}
+		public void setAge(int age) {	
+			this.age = age;
+		}
+		public String toString() {
+			return "OopsConceptsDemo [serial Number=" + serialNum +", name=" + name +",age=" + age+"]";
+		}
+	}
+	
